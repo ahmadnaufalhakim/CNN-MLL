a0da7c1b22855e37bb820af96e77dd3756441be4
Ahmad Naufal Hakim <ahmadnaufalhakim@gmail.com>
2020-09-20 00:06:41 +0700

    update main

--- main.py
+++ main.py
@@ -1,32 +1,34 @@
 from PIL import Image
 import numpy as np
-import convolutional as conv
-import pooling as pool
-import flatten as flat
-import dense as ds
-import sequential as seq
 import time
+
+from model.sequential import SequentialModel
+from model.layers.layer import Layer
+from model.layers.convolutional import Convolutional
+from model.layers.dense import Dense
+from model.layers.flatten import Flatten
+from model.layers.pooling import Pooling
 
 np.random.seed(0)
 
-# print(np.prod(np.zeros((1,2,3,4)).shape))
+IMG_DIR_TEST = './data/test'
+IMAGE_SIZE = (150, 150)
 
-model = seq.SequentialModel([
-  conv.Convolutional(96, (11, 11), (227, 227, 3), 0, 4),
-  pool.Pooling((3, 3), 2, 'max'),
-  conv.Convolutional(256, (5, 5), padding=2),
-  pool.Pooling((3, 3), 2, 'max'),
-  conv.Convolutional(384, (3, 3), padding=1),
-  conv.Convolutional(384, (3, 3), padding=1),
-  conv.Convolutional(256, (3, 3), padding=1),
-  pool.Pooling((3, 3), 2, 'max'),
-  flat.Flatten(),
-  ds.Dense(4096, 'relu'),
-  ds.Dense(4096, 'relu'),
-  ds.Dense(1000, 'sigmoid')
+test = tf.keras.preprocessing.image_dataset_from_directory(
+  IMG_DIR_TEST,
+  image_size=IMAGE_SIZE
+)
+
+model = SequentialModel([
+  Convolutional(4, (3, 3), (150, 150, 3), 0, 1),
+  Pooling((2, 2), 2),
+  Convolutional(8, (3, 3)),
+  Pooling((2, 2), 2),
+  Flatten(),
+  Dense(256, 'relu'),
+  Dense(1, 'sigmoid')
 ])
-
-image = Image.open("./data/test/cats/cat.9.jpg")
+image = Image.open("./data/test/cats/cat.15.jpg")
 dummy_array = np.array([[[85, 170, 255],
                          [170, 255, 85],
                          [255, 85, 170]],
@@ -42,99 +44,7 @@ dummy_array = np.array([[[85, 170, 255],
                         [[1, 2, 3],
                          [4, 5, 6],
                          [7, 8, 9]]])
+# print(dummy_array)
 # model.forward(image)
-# print(model.forward(image))
 print(model.output_shapes)
-# print(model)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ## input from image
-# image = Image.open("./data/test/cats/cat.9.jpg")
-# conv2d_img = conv.Convolutional(image, n_filters=3, filter_dim=3, padding=0, stride=1)
-# print('conv2d_img.input.shape:', conv2d_img.input.shape)
-# # conv2d_img.preprocess()
-# print('conv2d_img.input.shape:', conv2d_img.input.shape)
-# start = time.time()
-# conv2d_img.convolution()
-# end = time.time()
-# print(end-start)
-# conv2d_img.detector()
-# # Image.fromarray(conv2d_img.feature_maps.astype('uint8')).save('img.jpg')
-# print('conv2d_img.feature_maps:')
-# print(conv2d_img.feature_maps)
-# print(conv2d_img.feature_maps.shape)
-# pool2d_img = pool.Pooling(conv2d_img.feature_maps)
-# pool2d_img.pooling()
-# # Image.fromarray(pool2d_img.feature_maps.astype('uint8')).save('img_pool.jpg')
-# print('pool2d_img.feature_maps:')
-# print(pool2d_img.feature_maps)
-# print('pool2d_img.feature_maps.shape:', pool2d_img.feature_maps.shape)
-
-### input from array from image
-# array = np.array(image)
-# conv2d_arr = conv.ConvolutionalLayer(array, (300, 300), 3, 3, 5, 2)
-# print('conv2d_arr.input.shape:', conv2d_arr.input.shape)
-# conv2d_arr.preprocess()
-# print('conv2d_arr.input.shape:', conv2d_arr.input.shape)
-# conv2d_arr.convolution()
-# conv2d_arr.detector()
-# print('conv2d_arr.feature_maps:')
-# print(conv2d_arr.feature_maps)
-# print(conv2d_arr.feature_maps.shape)
-# pool2d_arr = pool.Pooling(conv2d_arr.feature_maps)
-# pool2d_arr.pooling()
-# print('pool2d_arr.feature_maps:')
-# print(pool2d_arr.feature_maps)
-# print('pool2d_arr.feature_maps.shape:', pool2d_arr.feature_maps.shape)
-
-# ## input from array (manual)
-# dummy_array = np.array([[[85, 170, 255], [170, 255, 85], [255, 85, 170]],
-#                         [[170, 255, 85], [255, 85, 170], [85, 170, 255]],
-#                         [[255, 85, 170], [85, 170, 255], [170, 255, 85]]])
-# conv2d = conv.Convolutional(dummy_array, (2, 2), 5, 2, 1, 1)
-# print('conv2d.input.shape:', conv2d.input.shape)
-# conv2d.preprocess()
-# print('conv2d.input.shape:', conv2d.input.shape)
-# conv2d.convolution()
-# conv2d.detector()
-# print('conv2d.feature_maps:')
-# print(conv2d.feature_maps)
-# print('conv2d.feature_maps.shape:', conv2d.feature_maps.shape)
-# pool2d = pool.Pooling(conv2d.feature_maps, stride=1)
-# pool2d.pooling()
-# print('pool2d.feature_maps:')
-# print(pool2d.feature_maps)
-# print('pool2d.feature_maps.shape:', pool2d.feature_maps.shape)
-# fl = flat.Flatten(pool2d.feature_maps)
-# print(fl.output)
-# d = ds.Dense(fl.output, 15, activation="relu")
-# print(d.weights.shape)
-# print(d.weights)
-# d.forward()
-# print(d.output)
-# d2 = ds.Dense(d.output, 1, activation="sigmoid")
-# d2.forward()
-# print(d2.output)
-
-# Image.fromarray(conv2d_img.feature_maps, 'RGB').save("conv2d_img.jpg")
-# Image.fromarray(conv2d_arr.feature_maps, 'RGB').save("conv2d_arr.jpg")
-# Image.fromarray(conv2d.feature_maps, 'RGB').save("dummy.jpg")
+# print(model.forward(arr))
